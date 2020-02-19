793c680eb928bc557a3b1b74299f9d9cdf1da76b
william <willchung42@gmail.com>
2020-02-18 23:33:15 -0500

    swift app

---
+++ SneakPic/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  SneakPic
+//
+//  Created by William on 2/18/20.
+//  Copyright © 2020 William. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
